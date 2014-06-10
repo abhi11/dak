9086d440675883ca70fd2f97f28591bcb60ec045
Abhishek Bhattacharjee <abhishek.bhattacharjee11@gmail.com>
2014-06-11 02:59:50 +0530

    Added xml part.

--- tools/appdata.py
+++ tools/appdata.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-'''Checks binaries with a .desktop file'''
+'''Checks binaries with a .desktop file or an appdata-xml file.
+   Generates a dict with package name and associated appdata in 
+   a list as value.'''
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-class desktop:
+class appdata:
     def __init__(self,connstr=None):
         ''' Initialize the variables and make the connection '''
     
@@ -17,6 +19,10 @@ class desktop:
             self._constr = ''
             self._engine = None
             self._session = None
+        self._deskdic = {}
+        self._xmldic = {}
+        self._commdic = {}
+        
 
     def create_session(self,connstr):
         ''' Establishes a session if it is not initialized during __init__. '''
@@ -28,7 +34,7 @@ class desktop:
             print "Connection string invalid!"
             return None
             
-    def find_bins(self,session = None):
+    def find_desktop(self,session = None):
         ''' Find binaries with a .desktop file. '''
         if session:
             self._session = session
@@ -36,18 +42,60 @@ class desktop:
         #select all the binaries that have a .desktop file
 
         sql = '''SELECT bc.file ,b.package from binaries b, bin_contents bc 
-        where bc.file like '%.desktop' and b.id = bc.binary_id'''
+        where bc.file like 'usr/share/applications/%.desktop' and b.id = bc.binary_id'''
+
         result = self._session.execute(sql)
         rows = result.fetchall()
+        #create a dict with package:[.desktop files]
         for r in rows:
-            print 'file: '+str(r[0])+ ' package: '+str(r[1])
+            try:
+                self._deskdic[str(r[1])].append(str(r[0]))
+            except KeyError:
+                self._deskdic[str(r[1])] = [str(r[0])]
+
+    def find_xml(self,session = None):
+        ''' Find binaries with a .xml file. '''
+
+        if session:
+            self._session = session
+        #SQL logic:
+        #select all the binaries that have a .xml file
+
+        sql = '''SELECT bc.file ,b.package from binaries b, bin_contents bc 
+        where bc.file like 'usr/share/appdata/%.xml' and b.id = bc.binary_id'''
+
+        result = self._session.execute(sql)
+        rows = result.fetchall()
+        #create a dict with package:[.xml files]
+        for r in rows:
+            try:
+                self._xmldic[str(r[1])].append(str(r[0]))
+            except KeyError:
+                self._xmldic[str(r[1])] = [str(r[0])]
+
+    def comb_appdata(self):
+        ''' Combines both dictionaries and creates a new list with all the metdata
+        (i.e the .desktop as well as .xml files. '''
+
+        #copy the .desktop files dict
+        self._commdic = self._deskdic
+
+        for key,li in self._xmldic.iteritems():
+            #if package already in list just append the xml files
+            try:
+                self._commdic[key] += li
+            #else create a new entry for the package
+            except KeyError:
+                self._commdic[key] = li
+
+    def printfiles(self):
+        ''' Prints the commdic '''
+        for k,l in self._commdic.iteritems():
+            print k +': ',l
 
 if __name__ == "__main__":
-    Desktop = desktop('postgresql+psycopg2://postgres:postgres@localhost/projectc')
-    Desktop.find_bins(session)
-            
-        
-        
-        
-
-
+    ap = appdata('postgresql+psycopg2://postgres:postgres@localhost/projectc')
+    ap.find_desktop()
+    ap.find_xml()
+    ap.comb_appdata()
+    ap.printfiles()
